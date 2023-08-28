3833f8017c0ac268eea0692a949bdf797d5962ea
MuhNurAqsalAminullah <aqsal3236@gmail.com>
2023-08-28 18:52:57 +0800

    five commit

--- config/database.js
+++ config/database.js
@@ -1,15 +1,20 @@
 const mysql = require("mysql2");
 
-const con = mysql.createConnection({
+const con = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-con.connect((err) => {
+con.getConnection((err) => {
   if (err) throw err;
-  console.log("Connected");
+  console.log("Connection...");
 });
+
+// con.connect((err) => {
+//   if (err) throw err;
+//   console.log(`Connected ${err}`);
+// });
 
 module.exports = con;
